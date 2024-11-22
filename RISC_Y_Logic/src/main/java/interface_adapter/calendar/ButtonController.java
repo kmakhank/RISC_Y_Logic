0915d387244ee392fc33661138455f88f4551e2c
Yiming Li <yimingli66@outlook.com>
2024-11-21 22:56:46 -0500

    Add interface for buttons in CalendarView.java, and implement CalendarBackButtonController.java

---
+++ RISC_Y_Logic/src/main/java/interface_adapter/calendar/ButtonController.java
@@ -0,0 +1,5 @@
+package interface_adapter.calendar;
+
+public interface ButtonController {
+    void click();
+}
--- RISC_Y_Logic/src/main/java/interface_adapter/calendar/CalendarBackButtonController.java
+++ RISC_Y_Logic/src/main/java/interface_adapter/calendar/CalendarBackButtonController.java
@@ -1,4 +1,9 @@
 package interface_adapter.calendar;
 
-public class CalendarBackButtonController {
+import view.MainMenuView;
+
+public class CalendarBackButtonController implements ButtonController {
+    public void click() {
+        new MainMenuView();
+    }
 }
